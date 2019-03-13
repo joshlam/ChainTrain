b2838586bb50c421e88478ccb1a4e0d75d49a538
Joshua Lam <joh641@gmail.com>
2019-03-13 01:11:21 -0700

    Add icons
    Adds icons for currencies/logo. React Native requires the image name to
    be known statically (i.e. can't just do something like
    require(`src/icons/${symbol}.png`)) so these require are done in an
    icons constant file. This allows fetching the image sources
    programatically still without hardcoding in any of the components (ex.
    ICONS[symbol]).
    
    https://facebook.github.io/react-native/docs/images

---
+++ src/styles/icons.js
@@ -0,0 +1,23 @@
+export default {
+  XBT: require('src/icons/XBT.png'),
+  USDT: require('src/icons/USDT.png'),
+  XRP: require('src/icons/XRP.png'),
+  XLM: require('src/icons/XLM.png'),
+  LTC: require('src/icons/LTC.png'),
+  XDG: require('src/icons/XDG.png'),
+  DASH: require('src/icons/DASH.png'),
+  ETH: require('src/icons/ETH.png'),
+  REP: require('src/icons/REP.png'),
+  ZEC: require('src/icons/ZEC.png'),
+  MLN: require('src/icons/MLN.png'),
+  XMR: require('src/icons/XMR.png'),
+  GNO: require('src/icons/GNO.png'),
+  EOS: require('src/icons/EOS.png'),
+  BCH: require('src/icons/BCH.png'),
+  BSV: require('src/icons/BSV.png'),
+  QTUM: require('src/icons/QTUM.png'),
+  ADA: require('src/icons/ADA.png'),
+  XTZ: require('src/icons/XTZ.png'),
+  ETC: require('src/icons/ETC.png'),
+  logo: require('src/icons/logo.png')
+};
