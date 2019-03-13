1e907d5588b16f782ec7b6489eb72035ecdfdd01
Joshua Lam <joh641@gmail.com>
2019-03-12 19:06:45 -0700

    Add TypeScript support
    Adds support for TypeScript in the app, tests, and linting.
    
    https://github.com/Microsoft/TypeScript-React-Native-Starter
    https://github.com/typescript-eslint/typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,31 @@ module.exports = {
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:react-native/all"
+    "plugin:react-native/all",
+    "plugin:@typescript-eslint/recommended"
   ],
   "globals": {
     "Atomics": "readonly",
     "SharedArrayBuffer": "readonly"
   },
-  "parser": "babel-eslint",
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true
     },
     "ecmaVersion": 2018,
+    "project": "./tsconfig.json",
     "sourceType": "module"
   },
   "plugins": [
     "react",
-    "react-native"
+    "react-native",
+    "@typescript-eslint"
   ],
   "rules": {
     "react/display-name": 0,
-    "react/prop-types": 0
+    "react/prop-types": 0,
+    "@typescript-eslint/indent": ["error", 2]
   },
   "settings": {
     "react": {
